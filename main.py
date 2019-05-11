6d70d404e61ba7256596b5670228cf9f1c72cc33
kenboo98 <tellambu@ualberta.ca>
2019-05-10 22:38:23 -0700

    Added slack integration

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests
+import os
+import slack
+
+
+def send_slack(is_test=False):
+    client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+    response = client.chat_postMessage(
+        channel='GHD8G6RKR',
+        text="The ESS Server is Down!" if not is_test else "This is a test message")
 
 def ess_check(request):
     """Responds to any HTTP request.
@@ -9,7 +18,12 @@ def ess_check(request):
         "CRIT" if the server isn't up
     """
     r = requests.get("https://ess.ualberta.ca")
+
+    if "test" in request.args:
+        send_slack(is_test=True)
+        return "TEST"
     if r.status_code == 200:
         return "OK"
     else:
+        send_slack()
         return "CRIT"
